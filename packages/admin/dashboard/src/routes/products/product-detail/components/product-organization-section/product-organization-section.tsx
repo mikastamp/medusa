53f60aaa6506bb2cd802a904ef34167cd69649b4
fPolic <mainacc.polic@gmail.com>
2025-01-09 09:35:12 +0100

    feat: product details sidebar, refactor sidebar link

--- packages/admin/dashboard/src/routes/products/product-detail/components/product-organization-section/product-organization-section.tsx
+++ packages/admin/dashboard/src/routes/products/product-detail/components/product-organization-section/product-organization-section.tsx
@@ -17,6 +17,8 @@ export const ProductOrganizationSection = ({
   const { t } = useTranslation()
   const { getDisplays } = useDashboardExtension()
 
+  console.log("product", product)
+
   return (
     <Container className="divide-y p-0">
       <div className="flex items-center justify-between px-6 py-4">
