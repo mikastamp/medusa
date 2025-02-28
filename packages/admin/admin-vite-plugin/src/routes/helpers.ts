64451edd979edaf69f7ac92fb340ca9b75b8f2b4
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2025-02-28 16:26:58 +0100

    update route helper

--- packages/admin/admin-vite-plugin/src/routes/helpers.ts
+++ packages/admin/admin-vite-plugin/src/routes/helpers.ts
@@ -4,7 +4,7 @@ export function getRoute(file: string): string {
   const importPath = normalizePath(file)
   return importPath
     .replace(/.*\/admin\/(routes)/, "")
-    .replace(/\[([^\]]+)\]/g, ":$1")
+    .replace(/(?:\[([^\]]+)\]|_([^_]+)_)/g, ":$1$2")
     .replace(
       new RegExp(
         `/page\\.(${VALID_FILE_EXTENSIONS.map((ext) => ext.slice(1)).join(
